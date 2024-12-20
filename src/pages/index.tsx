cbe16b6f37fc7aa9f9f8a58cff9ad1f784cc5d64
Alvaro <alvaroarpal@gmail.com>
2024-12-20 14:49:48 +0100

    Added pagination, redirect to homepage when not logged in, added invite button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
-import Main from "../components/Main";
-import Footer from "../components/Footer";
-import { ToastContainer, toast } from "react-toastify";
+import DiscordInvite from "@/components/DiscordInvite";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { getAvailableAudios, getGuildsByUser, playSound } from "@/services/apiService";
 
 const Home = () => {
-  const [guilds, setGuilds] = useState([]);
-  const [selectedGuild, setSelectedGuild] = useState("");
-  const [channels, setChannels] = useState([]);
-  const [audios, setAudios] = useState<string[]>([]);
-  const [selectedChannel, setSelectedChannel] = useState("");
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
-    if (session) {
-      const userId = session.user?.image?.split("/")[4];
-      getGuildsByUser(userId as any)
-        .then(setGuilds)
-        .catch(() => {
-          toast.error("Failed to load guilds");
-        });
+    if (status === "authenticated") {
+      router.push("/play"); // Redirect to /something if logged in
     }
-  }, [session]);
-
-  useEffect(() => {
-    getAvailableAudios()
-      .then(setAudios)
-      .catch(() => {
-        toast.error("Failed to load audios");
-      });
-  }, []);
-
-  useEffect(()=>{
-    setIsButtonDisabled(!selectedChannel || !selectedGuild)
-  },[selectedChannel, selectedGuild])
-
-  const handlePlaySound = async (audio: string) => {
-    if (isButtonDisabled) return;
-
-    setIsButtonDisabled(true);
-    try {
-      await playSound(selectedGuild, selectedChannel, audio);
-      toast.success("Sound is playing!", { position: "bottom-left" });
-    } catch (e) {
-      toast.error(`Failed to play sound! ${e}`, { position: "bottom-left" });
-    } finally {
-      setTimeout(() => setIsButtonDisabled(false), 1000);
-    }
-  };
+  }, [status, router]);
 
   return (
     <div className="min-h-screen bg-discord-dark text-white flex flex-col">
       <Header />
       <main className="flex-1 flex flex-col items-center justify-center text-center">
-        {session ? (
-          <Main
-            guilds={guilds}
-            selectedGuild={selectedGuild}
-            setSelectedGuild={setSelectedGuild}
-            channels={channels}
-            setChannels={setChannels}
-            selectedChannel={selectedChannel}
-            setSelectedChannel={setSelectedChannel}
-            handlePlaySound={handlePlaySound}
-            isButtonDisabled={isButtonDisabled}
-            audios={audios}
-          />
-        ) : (
-          <p>Please sign in to use this feature.</p>
-        )}
+        <DiscordInvite />
       </main>
-      <Footer />
       <ToastContainer />
     </div>
   );
---
+++ src/pages/play/index.tsx
@@ -0,0 +1,86 @@
+/* Test.tsx */
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import Footer from "../../components/Footer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { getAvailableAudios, getGuildsByUser, playSound } from "@/services/apiService";
+import { useRouter } from 'next/router';
+
+const Play = () => {
+  const [guilds, setGuilds] = useState([]);
+  const [selectedGuild, setSelectedGuild] = useState("");
+  const [channels, setChannels] = useState([]);
+  const [audios, setAudios] = useState<string[]>([]);
+  const [selectedChannel, setSelectedChannel] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const { data: session } = useSession();
+  const router = useRouter();
+
+
+  useEffect(() => {
+    if(session === null){
+        router.push('/');
+    }
+    if (session) {
+      const userId = session.user?.image?.split("/")[4];
+      getGuildsByUser(userId as any)
+        .then(setGuilds)
+        .catch(() => {
+          toast.error("Failed to load guilds");
+        });
+    }
+  }, [session, router]);
+
+  useEffect(() => {
+    getAvailableAudios()
+      .then(setAudios)
+      .catch(() => {
+        toast.error("Failed to load audios");
+      });
+  }, []);
+
+  useEffect(() => {
+    setIsButtonDisabled(!selectedChannel || !selectedGuild);
+  }, [selectedChannel, selectedGuild]);
+
+  const handlePlaySound = async (audio: string) => {
+    if (isButtonDisabled) return;
+
+    setIsButtonDisabled(true);
+    try {
+      await playSound(selectedGuild, selectedChannel, audio);
+      toast.success("Sound is playing!", { position: "bottom-left" });
+    } catch (e) {
+      toast.error(`Failed to play sound! ${e}`, { position: "bottom-left" });
+    } finally {
+      setTimeout(() => setIsButtonDisabled(false), 1000);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-discord-dark text-white flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center text-center">
+        <Main
+          guilds={guilds}
+          selectedGuild={selectedGuild}
+          setSelectedGuild={setSelectedGuild}
+          channels={channels}
+          setChannels={setChannels}
+          selectedChannel={selectedChannel}
+          setSelectedChannel={setSelectedChannel}
+          handlePlaySound={handlePlaySound}
+          isButtonDisabled={isButtonDisabled}
+          audios={audios}
+        />
+      </main>
+      <Footer />
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Play;
